f91dee42bb1b31ba1ef555ecd404f6bb63391d29
ArunachalamTringapps <arunachalam.muthuvel@tringapps.com>
2023-02-12 20:49:36 +0530

    updated

--- src/main/java/org/example/Frequency.java
+++ src/main/java/org/example/Frequency.java
@@ -1,7 +1,43 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.*;
+import java.util.logging.*;
+import java.util.*;
+
+class Frequency
+{
+    static void count(String str)
+    {
+        Map<String,Integer>hm=new HashMap<>();
+        String []arr=str.split("[ .,]+");
+        for (String s : arr) {
+            if (hm.containsKey(s)) {
+                hm.put(s, hm.get(s) + 1);
+            } else {
+                hm.put(s, 1);
+            }
+        }
+        Logger l= Logger.getLogger("arun");
+        Queue<Map.Entry<String,Integer>> pq=new PriorityQueue<>((a,b)-> b.getValue()-a.getValue());
+        pq.addAll(hm.entrySet());
+        while(!pq.isEmpty())
+        {
+
+            l.log(Level.INFO,()-> ""+pq.poll());
+        }
+
+    }
+    public static void main(String...args) throws IOException {
+        File file=new File("src/main/java/org/example/inputs.txt");
+        BufferedReader br=new BufferedReader(new FileReader(file));
+        String nextline;
+        StringBuilder sb=new StringBuilder();
+        while ((nextline=br.readLine())!=null){
+            sb.append(nextline);
+        }
+        String newString=sb.toString().replaceAll("\n", " ");
+        Frequency.count(newString);
+
+
     }
 }
