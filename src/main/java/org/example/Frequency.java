72ef1416673b91e665162bd0ec3751f103f55d65
ArunachalamTringapps <arunachalam.muthuvel@tringapps.com>
2023-03-01 15:53:07 +0530

    updated

--- src/main/java/org/example/Frequency.java
+++ src/main/java/org/example/Frequency.java
@@ -1,16 +1,14 @@
 package org.example;
 
-import java.io.*;
-import java.util.logging.*;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-class Frequency
-{
-    static Logger l= Logger.getLogger(Frequency.class.getName());
-    static void count(String str)
-    {
+public class Frequency {
+    private static void count(String word){
+        Logger l=Logger.getLogger(Frequency.class.getName());
         Map<String,Integer>hm=new HashMap<>();
-        String []arr=str.split("[ .,]+");
+        String []arr=word.split("[ .,]+");
         for (String s : arr) {
             if (hm.containsKey(s)) {
                 hm.put(s, hm.get(s) + 1);
@@ -18,7 +16,6 @@ class Frequency
                 hm.put(s, 1);
             }
         }
-
         Queue<Map.Entry<String,Integer>> pq=new PriorityQueue<>((a,b)-> b.getValue()-a.getValue());
         pq.addAll(hm.entrySet());
         while(!pq.isEmpty())
@@ -27,18 +24,13 @@ class Frequency
             l.log(Level.INFO,()-> ""+pq.poll());
         }
 
-    }
-    public static void main(String...args) throws IOException {
-        File file=new File("src/main/java/org/example/inputs.txt");
-        BufferedReader br=new BufferedReader(new FileReader(file));
-        String nextline;
-        StringBuilder sb=new StringBuilder();
-        while ((nextline=br.readLine())!=null){
-            nextline=nextline.concat(",");
-            sb.append(nextline);
-        }
-        l.log(Level.INFO,()->""+sb);
-        Frequency.count(sb.toString());
+}
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Logger l= Logger.getLogger(Frequency.class.getName());
+        l.info("Enter the String");
+        String word=sc.nextLine();
+        count(word);
 
     }
 }
